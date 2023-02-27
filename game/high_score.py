fc928ef70fc607f628dcae89bfc2ea1d62fbf10b
Olof Thornell <olof.ot.thornell@gmail.com>
2023-02-27 19:17:11 +0100

    Added methods in player and fixed issues in high_score

--- game/high_score.py
+++ game/high_score.py
@@ -1,24 +1,22 @@
-import player
+from player import Player
 import pickle
 
 
-class highscore():
+class Highscore():
 
     def __init__(self):
         high_score_file = open('high_score_file.bin', 'rb')
         high_score_dict = pickle.load(high_score_file)
         high_score_file.close()
 
-
-    def get_high_score(high_score_file):
-        for i in high_score_file:
-            print(high_score_file[i])
-
-
+    def get_high_score(self, high_score_dict):
+        for i in high_score_dict:
+            print(high_score_dict[i])
 
     def save(self, high_score_dict):
+        name = Player.get_name()
+        score = Player.get_score()
         high_score_dict[name] = score
         high_score_file = open('high_score_file.bin', 'wb')
         pickle.dump(high_score_dict, high_score_file)
         high_score_file.close()
-
--- game/player.py
+++ game/player.py
@@ -1,7 +1,17 @@
-class player():
+class Player():
 
-    def __init__(self, name):
+    def __init__(self, name, score):
         self.name = name
+        self.score = 0
 
     def change_name(self, new_name):
-        name = new_name
+        self.name = new_name
+
+    def get_name(self):
+        return self.name
+
+    def get_score(self):
+        return self.score
+
+    def set_score(self, new_score):
+        self.score = new_score
