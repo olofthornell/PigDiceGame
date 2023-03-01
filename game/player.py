57cec35c7cc0ec57dac37bf8c1d109a1f088d66f
Olof Thornell <olof.ot.thornell@gmail.com>
2023-03-01 12:18:39 +0100

    Added getters and setters for Scores and removed Scores parameters from constructor

--- game/player.py
+++ game/player.py
@@ -1,6 +1,6 @@
 class Player():
 
-    def __init__(self, name, wins, roll_score, turn_score, total_score):
+    def __init__(self, name):
         self.name = name
         self.wins = 0
         self.roll_score = 0
@@ -13,8 +13,26 @@ class Player():
     def set_name(self, new_name):
         self.name = new_name
 
-    def get_score(self):
+    def get_wins(self):
         return self.score
 
-    def set_score(self, new_score):
+    def set_wins(self, new_score):
         self.score = new_score
+
+    def get_roll_score(self):
+        return self.roll_score
+
+    def set_roll_score(self, new_roll_score):
+        self.roll_score = new_roll_score
+
+    def get_turn_score(self):
+        return self.turn_score
+
+    def set_turn_score(self, new_turn_score):
+        self.turn_score = new_turn_score
+
+    def get_total_score(self):
+        return self.total_score
+
+    def set_total_score(self, new_total_score):
+        self.total_score = new_total_score
