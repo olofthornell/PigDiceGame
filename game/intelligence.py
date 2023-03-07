8d77f3c91dd7373ef2f3228f210d17cf27eadeab
Rickard Sturesson <karl.rickard.sturesson@gmail.com>
2023-03-07 10:15:05 +0100

    Worked on computer player

--- game/intelligence.py
+++ game/intelligence.py
@@ -1,14 +1,22 @@
-class intelligence:
+"""Holds the personality of the cpu player"""
+
+
+class Intelligence:
+    """Cpu personality"""
     
-    def __init__(self, difficulty):
-        self.coward = False
-        self.moderate = True
-        self.bold = False
-    
+    def __init__(self):
+        self._coward = 4
+        self._moderate = 15
+        self._bold = 25
+
     def coward(self):
-        if self.player.turn_score > 6:
-            self.game.hold()
-            
+        """A cpu player that is a coward"""
+        return self._coward
+
     def moderate(self):
-        if self.player.turn_score > 15:
-            self.game.hold()
+        """A cpu player that is moderate"""
+        return self._moderate
+
+    def bold(self):
+        """A cpu player that is bold"""
+        return self._bold
