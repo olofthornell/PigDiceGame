4b49107a4af82cb5e10252735ee367c395c914a6
Olof Thornell <olof.ot.thornell@gmail.com>
2023-03-09 22:25:38 +0100

    test_player finished

---
+++ game/test_high_score.py
@@ -0,0 +1,11 @@
+"""Testing high_score class."""
+import unittest
+import high_score
+
+
+def test_init_default_object(self):
+    """Init high_score test."""
+
+
+if __name__ == "__main__":
+    unittest.main()
