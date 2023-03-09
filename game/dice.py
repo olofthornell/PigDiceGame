2a1f795bec8a63cd44b90fb0ec909aff70722b72
Rickard Sturesson <karl.rickard.sturesson@gmail.com>
2023-03-09 21:25:37 +0100

    Docstrings in dice

--- game/dice.py
+++ game/dice.py
@@ -1,9 +1,9 @@
-"""The dice"""
+"""The dice."""
 import random
 
 
 class Dice:
-    """A class that holds information about a dice"""
+    """A class that holds information about a dice."""
 
     def __init__(self):
         """Init the object."""
@@ -11,13 +11,13 @@ class Dice:
         self._dice_max = 6
 
     def dice_min(self):
-        """Min value of the dice"""
+        """Min value of the dice."""
         return self._dice_min
 
     def dice_max(self):
-        """Min value of the dice"""
+        """Min value of the dice."""
         return self._dice_max
 
     def roll(self):
-        "Get a value of the dice between 1 and 6"
+        """Get a value of the dice between 1 and 6."""
         return random.randint(1, 6)
